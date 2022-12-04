6ca00af6a015ec58ec7556d58c4b338dd70e61e1
Adrian Rodriguez <zapxcero@gmail.com>
2022-12-04 15:28:52 +0800

    Home root to Events#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  get 'events/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "events#index"
 end
