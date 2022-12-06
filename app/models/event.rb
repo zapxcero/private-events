3017820033d1e91973d429957d4e5714f89d6d2d
Adrian Rodriguez <zapxcero@gmail.com>
2022-12-06 16:34:29 +0800

    Add: Past/Future scope for Event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,4 +4,7 @@ class Event < ApplicationRecord
   has_many :attendee, through: :attendance, source: :attendee
   has_many :attendances, foreign_key: :attended_event_id
   belongs_to :creator, class_name: 'User'
+
+  scope :past, -> { where("date < ?", Time.now) }
+  scope :future, -> { where("date > ?", Time.now) }
 end
